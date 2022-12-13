2a6db179c7af66cf2834658af79e21575ed205e6
laurelamothe <laurelamothel@gmail.com>
2022-12-14 00:51:45 +0100

    barplot scripts

---
+++ scripts/barplot_v2.R
@@ -0,0 +1,31 @@
+## A - Chargement du jeu de données
+
+df = read.csv(file="raw_data/quantification_by_contig_lineage_all_reduced.csv",
+              sep="\t")
+
+
+## sommer les abondances relatives de toutes les ligne du même genre
+
+
+rm(dat_genre, var, var2, test)
+dat_genre= data.frame(0)
+
+test=0
+for (var in c(1:dim.data.frame(df)[1])){
+  for (var2 in c(1:dim.data.frame(dat_genre)[2])){
+    if (df[var,]$genre == dat_genre[1,var2]){
+      dat_genre[2,var2]=as.integer(dat_genre[2,var2]) + as.integer(df[var,]$nb_reads_Holopig_colon01_quantif_percontig)  
+      test=0
+      break
+    }
+    else {test=1}
+  }   
+  if (test==1){
+    dat_genre= cbind(dat_genre, c(df[var,]$genre , df[var,]$nb_reads_Holopig_colon01_quantif_percontig))
+    colnames(dat_genre[var2])= df[var,]$genre
+  test = 0
+  }
+}
+colnames(dat_genre)= dat_genre[1,]
+
+
