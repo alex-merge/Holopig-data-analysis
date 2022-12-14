8027fff22ac12904f492eb90f79d0391f4bab193
PieMarchal <92150660+PieMarchal@users.noreply.github.com>
2022-12-14 18:47:08 +0100

    update function_reduced.R
    change the scale of the relative abundance

--- scripts/Function_reduced_data.R
+++ scripts/Function_reduced_data.R
@@ -10,7 +10,12 @@ data
 #barplot(dict_function)#this is a first test
 
 #functions_table = data.frame(data[2:36], data$Description)
-function_table = data.frame(data[2:35], data$Description)
+function_table = data.frame(data[2:35], data$Description, data$sum)
+
+
+
+
+
 categorie = c("Control", "Colistine", "Control", "Control", "Control", "Control",
               "Colistine", "Colistine", "Control", "Colistine", "Colistine", "Control",
               "Control", "Control", "Control", "Control", "Control", "Colistine",
@@ -23,21 +28,57 @@ metabolic_function = c()
 animal = c()
 relative_abundance = c()
 categorie_concerne = c()
+#somme = c()
 
-for (i in 1:(length(function_table)-1)){
+
+
+for (i in 1:(length(function_table)-2)){
+  
+  verif_func = c()
+  verif_pig =c()
+  verif_sum = c()
+  verif_animal = c()
+  verif_categorie = c()
+  
 
   for (j in 1:length(function_table[,i])){
-    function_table[j,i] = function_table[j,i]/data$sum[j]
+    #function_table[j,i] = function_table[j,i]/data$sum[j]
     
-    metabolic_function=c(metabolic_function,data$Description[j])
-    animal=c(animal,paste0("p",i))
-    relative_abundance=c(relative_abundance,function_table[j,i])
+    #somme = c(somme, function_table$data.sum[j]) 
+    #metabolic_function=c(metabolic_function, function_table$data.Description[j])
+    #animal=c(animal,paste0("p",i))
+    #relative_abundance=c(relative_abundance,function_table[j,i])
+    #categorie_concerne = c(categorie_concerne,categorie[i])
     
-    categorie_concerne = c(categorie_concerne,categorie[i])
+    if (function_table$data.Description[j] %in% verif_func){
+      posiO = match(function_table$data.Description[j],verif_func)
+      verif_pig[posiO] =c(verif_pig[posiO] + function_table[j,i])
+      verif_sum[posiO] =c(verif_sum[posiO] + function_table$data.sum[j])
+      
+    }
+    else {
+      verif_func = c(verif_func,function_table$data.Description[j])
+      verif_pig =c(verif_pig,function_table[j,i])
+      verif_sum = c(verif_sum,function_table$data.sum[j])
+      verif_animal = c(verif_animal,paste0("p",i))
+      verif_categorie = c(verif_categorie,categorie[i])
+    }
   }
-
+  verif_pig_relative = verif_pig / verif_sum# pas sûr que ce soit okk sur le jeu entier, à tester
+  #somme = c(somme, verif_sum) 
+  metabolic_function=c(metabolic_function, verif_func)
+  animal=c(animal,verif_animal)
+  relative_abundance=c(relative_abundance,verif_pig_relative)
+  categorie_concerne = c(categorie_concerne,verif_categorie)
+  
 }
+
+
+
 test = data.frame(animal,metabolic_function,relative_abundance,categorie_concerne)
+
+
+################################################################################
 
 # Library
 library(ggplot2)
