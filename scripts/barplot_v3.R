4067e0d7d5821db6bc93226bcdc9b2ea6dbe13a0
laurelamothe <laurelamothel@gmail.com>
2022-12-27 12:12:46 +0100

    Update barplot_v3.R

--- scripts/barplot_v3.R
+++ scripts/barplot_v3.R
@@ -31,3 +31,33 @@ abondances= c(t(dat_genre[2,]))
 
 
 barplot(t(abondances))
+
+
+#barplot des règnes
+
+rm(dat_regne, var, var2, test, abondances)
+dat_regne= data.frame(0)
+
+test=0
+for (var in c(1:dim.data.frame(df)[1])){
+  for (var2 in c(1:dim.data.frame(dat_regne)[2])){
+    if (df[var,]$regne == dat_regne[1,var2]){
+      dat_regne[2,var2]=as.integer(dat_regne[2,var2]) + as.integer(df[var,]$nb_reads_Holopig_colon01_quantif_percontig)  
+      test=0
+      break
+    }
+    else {test=1}
+  }   
+  if (test==1){
+    dat_regne= cbind(dat_regne, c(df[var,]$regne , df[var,]$nb_reads_Holopig_colon01_quantif_percontig))
+    colnames(dat_regne[var2])= df[var,]$regne
+    test = 0
+  }
+}
+colnames(dat_regne)= dat_regne[1,]
+abondances= c(t(dat_regne[2,]))
+
+
+barplot(t(abondances), names.arg = colnames(dat_regne))
+abondances_hors_bacteries= abondances[-3]
+barplot(t(abondances_hors_bacteries), names.arg = colnames(dat_regne[-3]))
