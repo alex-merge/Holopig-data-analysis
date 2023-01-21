7628f1d6f899ea382fa00a6dc273831cc09ae8b1
laurelamothe <laurelamothel@gmail.com>
2023-01-21 11:50:56 +0100

    Update Q4_tableau1.R

--- scripts/Q4_tableau1.R
+++ scripts/Q4_tableau1.R
@@ -5,7 +5,6 @@ gene_cor_df = read.csv(file="refined_data/filtered_metabo_X_gene_cor.csv",
                    stringsAsFactors = TRUE,
                    nrow = 100000,
                    na.strings = c("NA", "-"))
-colnames(gene_cor_df) =c("Metabolite",  "Gene", "Correlation")
 
 
 #lire tableau des gènes
@@ -23,17 +22,17 @@ colnames(gene_df) = c("Contig", "Gene")
 # conserver les lignes pour lesquelles le nom de gène se trouve dans gene_cor
 gene_contig_filtered = subset(gene_df, Gene %in% gene_cor_df$Gene)
 
-
-
-
+#suprimer les CDS pour ne garder que le nom du contig
+splitted_CDS = strsplit(as.character(gene_contig_filtered$Contig), ".C")
+gene_contig_filtered =  data.frame(t(data.frame(splitted_CDS)),gene_contig_filtered)
+gene_contig_filtered =  data.frame(gene_contig_filtered[,1], gene_contig_filtered[,4])
+colnames(gene_contig_filtered) = c("Contig", "Gene")
 
 library(tidyr)
 library(dplyr) 
 
 # ajouter la colonne métabolite (duplique les ligne pour chaque metabolite corrélé)
 gene_contig_meta = merge(gene_contig_filtered, gene_cor_df, group_by=  gene_contig_filtered$gene, all.x=TRUE)
-
-
 
 
 # trier les lignes par ordre décroissant de facteur de correlation
